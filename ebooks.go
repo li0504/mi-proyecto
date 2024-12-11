58b0c35caa2345ea676a507961cdd870283c614c
li0504 <liriovillon1920@gmail.com>
2024-12-10 23:35:50 -0500

    Add files via upload

---
+++ ebooks.go
@@ -0,0 +1,9 @@
+package datos
+
+// Estructura para representar un libro electrónico
+type Ebook struct {
+	ID     int
+	Titulo string
+	Autor  string
+	Link   string
+}
---
+++ usuarios.go
@@ -0,0 +1,8 @@
+package funciones
+
+import "fmt"
+
+// Función para mostrar un saludo personalizado (puedes añadir más lógica de usuarios aquí)
+func SaludarUsuario() {
+	fmt.Println("Bienvenido al Sistema de Gestión de Libros Electrónicos.")
+}
